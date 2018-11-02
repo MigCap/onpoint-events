6b20c3d0aaf1d781e4cc6ac131542d627d79ea90
Miguel C <MiguelC@iMac.local>
2018-11-02 01:13:09 +0100

    dashboard and form control for events

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
-
+import React, { Component, Fragment } from 'react'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Onpoint Events</h1>
-      </div>
-    );
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/layout/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+import App from './app/layout/App'
 // import * as serviceWorker from "./serviceWorker";
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root')
 
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
-};
-
-if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
-    setTimeout(render);
-  });
+  ReactDOM.render(<App />, rootEl)
 }
 
-render();
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render)
+  })
+}
+
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
